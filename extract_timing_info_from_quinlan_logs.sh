17e45cd0c885bc71e15a99ed0999ec6877fadda1
Dale McConachie <dmcconac@umich.edu>
2017-10-28 15:31:44 -0400

    Testing for ISRR final submission.
    Switched to non-PRM based sampling.
    Changed band update method to use alternate direction of point motion.

---
+++ extract_timing_info_from_quinlan_logs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+EXPERIMENT="cloth_single_pole"
+#EXPERIMENT="cloth_double_slit"
+#EXPERIMENT="rope_maze"
+#EXPERIMENT="rope_maze_from_top"
+TRIAL_DIR="isrr_quinlan_band"
+TRIAL_PREFIX="trial_"
+
+OUTPUT_DIR="${EXPERIMENT}/${TRIAL_DIR}"
+GREP_DIR="${EXPERIMENT}/${TRIAL_DIR}/${TRIAL_PREFIX}*"
+
+roscd smmap/logs
+
+grep -r "total_samples"                                             ${GREP_DIR} | grep -o "[0-9]*$" > ${OUTPUT_DIR}/planning_total_samples.txt
+grep -r "total_states"                                              ${GREP_DIR} | grep -o "[0-9]*$" > ${OUTPUT_DIR}/planning_total_states.txt
+grep -r "planning0_sampling_time"                                   ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/planning_sampling_time.txt
+grep -r "planning1_nearest_neighbour_time"                          ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/planning_nn_time.txt
+grep -r "planning2_forward_propogation_band_sim_time"               ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/planning_band_sim_time.txt
+grep -r "planning3_forward_propogation_first_order_vis_time"        ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/planning_first_order_vis_time.txt
+grep -r "planning4_forward_propogation_everything_included_time"    ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/planning_forward_prop_everything_time.txt
+grep -r "planning5_total_time"                                      ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/planning_total_time.txt
+grep -r "smoothing0_failed_iterations"                              ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/smoothing_failed_iterations.txt
+grep -r "smoothing1_iterations"                                     ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/smoothing_iterations_time.txt
+grep -r "smoothing2_forward_propogation_band_sim_time"              ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/smoothing_band_sim_time.txt
+grep -r "smoothing3_forward_propogation_first_order_vis_time"       ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/smoothing_first_ordeer_vis_time.txt
+grep -r "smoothing4_forward_propogation_everything_included_time"   ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/smoothing_forward_prop_everything_time.txt
+grep -r "smoothing5_total_time"                                     ${GREP_DIR} | grep -o "[0-9]*\.*[0-9]*$" > ${OUTPUT_DIR}/smoothing_total_time.txt
