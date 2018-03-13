20dc36004d475e0f6a8baefe717d3de3024f4c05
Dale McConachie <dmcconac@umich.edu>
2018-03-13 12:49:15 -0400

    Updated script to add video generation.

--- Mengyao/IROS/Matlab_Script/model_accuracy_plots.m
+++ Mengyao/IROS/Matlab_Script/model_accuracy_plots.m
@@ -5,41 +5,93 @@ space_hold_2 = '%n %n';
 
 % basename = '/home/dmcconac/Dropbox/catkin_ws/src/smmap/logs/';
 basename = 'E:/Dropbox/catkin_ws/src/smmap/logs/';
-folders = {
-    'rope_table_linear_plus_x/model_accuracy',
-    'rope_table_linear_minus_x/model_accuracy',
-%     'rope_table_linear_plus_y/model_accuracy',
-%     'rope_table_linear_minus_y/model_accuracy',
-%     'rope_table_linear_plus_z/model_accuracy',
-%     'rope_table_linear_pull_down_into_table/model_accuracy'
-    };
+folder = 'rope_table_IROS_model_accuracy_test/model_accuracy/';
 
-titles = {
-    'Pushing towards rope',
-    'Pulling away from rope'
-    };
 %%
 %%%%%%%%%%% Error Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 parameter_set_mm = 'New';
 parameter_set_dd_wo = 'Baseline';
 
-for ind = 1:length(folders)
-    folder = folders{ind};
-    error_filename = [basename, folder, '/model_prediction_error_unweighted.txt']
-    time_filename = [basename, folder, '/control_time.txt'];
-    image_path = [basename, folder, '/model_accuracies.png'];
+error_filename = [basename, folder, 'model_prediction_error_unweighted.txt'];
+time_filename = [basename, folder, 'control_time.txt'];
+image_path = [basename, folder, 'model_accuracies.png'];
+
+[constrained_model_error_squared, diminishing_rigidity_model_error_squared] = ...
+    textread(error_filename, space_hold_2, 'headerlines', 1);
+constrained_model_error = sqrt(constrained_model_error_squared);
+diminishing_rigidity_model_error = sqrt(diminishing_rigidity_model_error_squared);
+time = textread(time_filename, space_hold_1, 'headerlines', 1);
+time = time - time(1);
+
+fig = figure(1);
+plot(time, constrained_model_error,...
+     time, diminishing_rigidity_model_error, 'LineWidth', 2)
+legend(parameter_set_mm, parameter_set_dd_wo, 'Location', 'northeast')
+
+xlabel('Time (s)')
+ylabel('Model Prediction Error')
+saveas(fig, image_path)
+
+%%
+%%%%%%%%%%%%% Video generation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+make_video = true;
+video_path = [basename, folder, 'model_accuracies_video'];
+pause_len = time(2) - time(1);
+
+if make_video
+    try
+        votype = 'avifile';
+        vo = avifile([video_path '.avi'], 'fps', 1/pause_len);
+    catch
+        votype = 'VideoWriter';
+        vo = VideoWriter(video_path, 'Motion JPEG AVI');
+        set(vo, 'FrameRate', 1/pause_len);
+        open(vo);
+    end
     
-    [constrained_model_error, diminishing_rigidity_model_error] = ...
-        textread(error_filename, space_hold_2, 'headerlines', 1);
-    time = textread(time_filename, space_hold_1, 'headerlines', 1);
     
-    fig = figure(ind);
-    plot(time, sqrt(constrained_model_error),...
-         time, sqrt(diminishing_rigidity_model_error),'LineWidth',2)
+    plot(-1, -1, -1, -1, 'LineWidth', 2)
+    axis([0   10.0000         0    0.0150]);
     legend(parameter_set_mm, parameter_set_dd_wo, 'Location', 'northeast')
-%     title(titles{ind})
     xlabel('Time (s)')
     ylabel('Model Prediction Error')
-    saveas(fig, image_path)
+    
+    F = getframe(gcf);
+    
+    switch votype
+        case 'avifile'
+            vo = addframe(vo, F);
+        case 'VideoWriter'
+            writeVideo(vo, F);
+        otherwise
+            error('unrecognized votype');
+    end
+    
+    
+    
+    
+    for ind = 1:length(time)    
+        plot(time(1:ind), constrained_model_error(1:ind), ...
+             time(1:ind), diminishing_rigidity_model_error(1:ind), 'LineWidth', 2)
+        
+        axis([0   10.0000         0    0.0150]);
+        legend(parameter_set_mm, parameter_set_dd_wo, 'Location', 'northeast')
+        xlabel('Time (s)')
+        ylabel('Model Prediction Error')
+        drawnow;
+        
+        F = getframe(gcf);
+        
+        switch votype
+            case 'avifile'
+                vo = addframe(vo, F);
+            case 'VideoWriter'
+                writeVideo(vo, F);
+            otherwise
+                error('unrecognized votype');
+        end
+    end
+    
+    close(vo);
 end
