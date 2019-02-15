1f13c0471c38d5a984bcfa087e9dedacaa548454
Dale McConachie <dmcconac@umich.edu>
2019-02-15 13:29:24 -0500

    PressAnyKey function. Changed default store location for transition learning data.

--- deformable_manipulation_experiment_params/include/deformable_manipulation_experiment_params/utility.hpp
+++ deformable_manipulation_experiment_params/include/deformable_manipulation_experiment_params/utility.hpp
@@ -2,6 +2,7 @@
 
 #include <type_traits>
 #include <cstdint>
+#include <arc_utilities/arc_helpers.hpp>
 
 namespace smmap
 {
@@ -24,5 +25,26 @@ namespace smmap
             i = -i;
         }
         return i > 0 ? (int)std::log10((double)i) + 1 : 1;
+    }
+
+    inline int PressKeyToContinue(const std::string& message = "Press any key to continue ")
+    {
+        if (!arc_helpers::IsDebuggerPresent())
+        {
+            std::cout << message << std::flush;
+            auto key = arc_helpers::GetChar();
+            if (key != '\n')
+            {
+                std::cout << std::endl;
+            }
+            return key;
+        }
+        else
+        {
+            std::cout << "Process is under debugger, use breakpoints for "
+                      << "interactive flow control instead. Message: "
+                      << message << std::endl;
+            return '\n';
+        }
     }
 }
