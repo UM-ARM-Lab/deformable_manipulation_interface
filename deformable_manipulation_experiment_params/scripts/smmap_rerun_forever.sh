79f89c71c1a392fe7ac8849da4c61d35aaf2b62c
Dale McConachie <dmcconac@umich.edu>
2019-08-21 18:05:16 -0400

    Cleaner rerunning script.

--- deformable_manipulation_experiment_params/scripts/smmap_rerun_forever.sh
+++ deformable_manipulation_experiment_params/scripts/smmap_rerun_forever.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-while true
+TASKTYPE=rope_hooks_simple
+TESTID=generate_training_examples
+
+TERMINATE_FLAG_FILE=`rospack find deformable_manipulation_experiment_params`/scripts/rerun_terimnate_flag
+LOG_DIR=`rospack find smmap`/logs/$TASKTYPE/$TESTID
+mkdir -p $LOG_DIR
+
+while [[ ! -f "$TERMINATE_FLAG_FILE" ]];
 do
-    roslaunch deformable_manipulation_experiment_params generic_experiment.launch task_type:=rope_hooks start_bullet_viewer:=false disable_all_visualizations:=true use_random_seed:=true "$@"
+    STAMP=`date +"%s"`
+    roslaunch deformable_manipulation_experiment_params generic_experiment.launch \
+        task_type:=$TASKTYPE \
+        start_bullet_viewer:=false \
+        disable_smmap_visualizations:=true \
+        use_random_seed:=true \
+        classifier_type:=none \
+        test_id:=$TESTID \
+        rrt_num_trials:=1 \
+        test_paths_in_bullet:=true \
+        rerun_forever:=true \
+        "$@" --screen &> $LOG_DIR/$STAMP.log
 done
